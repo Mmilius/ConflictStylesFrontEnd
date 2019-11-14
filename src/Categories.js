3b36a895a15121f0ed11bea0db63c86340b5c3aa
github username <github email address>
2019-11-14 14:48:54 -0700

    add style images and content

--- src/Categories.js
+++ src/Categories.js
@@ -5,8 +5,14 @@ export default function Categories(props){
   function categoryList(){
       const {categories} = props
       return categories.map(category => {
-      return <li>
-          {category.category}
+      return <li className="style-headline">
+          <h2>{category.category}</h2>
+          <img className="style-image" src={category.image}></img>
+          <p>{category.description}</p>
+          <p>{category.bullets}</p>
+          <p>{category.uses}</p>
+          <p>{category.limits}</p>
+          {/* <img className="style-image" src={category.image}></img> */}
       </li>
   })    
 }  
